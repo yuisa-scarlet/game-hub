a83958de45e82e250f6678ba919f83d548ba19a5
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2025-03-12 01:49:02 +0700

    feat: custom hook for fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,35 +1,9 @@
-import { useEffect, useState } from "react";
+import useGames from "@/hooks/useGames";
 import { Text } from "@chakra-ui/react";
-import axios from "../services/axios";
-
-interface Game {
-  id: number;
-  slug: string;
-  name: string;
-  release: string;
-  tba: boolean;
-  background_image: string;
-  rating: number;
-  rating_top: number;
-  ratings_count: number;
-  reviews_text_count: number;
-}
-
-interface GameResponse {
-  count: number;
-  results: Game[];
-}
 
 export default function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    axios
-      .get<GameResponse>("/games")
-      .then((response) => setGames(response.data.results))
-      .catch((err) => setError(err.message));
-  });
+  
+  const { games, error } = useGames();
 
   return (
     <div>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,44 @@
+import axios from "@/services/axios";
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface Game {
+  id: number;
+  slug: string;
+  name: string;
+  release: string;
+  tba: boolean;
+  background_image: string;
+  rating: number;
+  rating_top: number;
+  ratings_count: number;
+  reviews_text_count: number;
+}
+
+interface GameResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    axios
+      .get<GameResponse>("/games", { signal: controller.signal })
+      .then((response) => setGames(response.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
