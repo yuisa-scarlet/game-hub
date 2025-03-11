dc5ebce604ab8e486021157df8887f2c6489093d
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2025-03-12 01:16:15 +0700

    feat: add color mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import AppLogo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 export default function Navbar() {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="1rem">
       <Image src={AppLogo} boxSize="60px" />
-      <Text>Navbar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+
 import App from "./App.tsx";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>
