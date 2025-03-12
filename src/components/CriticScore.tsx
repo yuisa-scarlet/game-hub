0b8bcdde039c8e34811e39cabb613646fb054823
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2025-03-12 14:17:42 +0700

    feat: optimize image in game card

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 export default function CriticScore({ score }: Props) {
-  let color = score >= 75 ? "green" : score >= 50 ? "yellow" : "red";
+  const color = score >= 75 ? "green" : score >= 50 ? "yellow" : "red";
 
   return (
     <Badge fontSize="14px" paddingX={2} borderRadius={4} colorScheme={color}>
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from "@/hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "@/services/image";
 
 interface Props {
   game: Game;
@@ -10,7 +11,7 @@ interface Props {
 export default function GameCard({ game }: Props) {
   return (
     <Card borderRadius={8} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
